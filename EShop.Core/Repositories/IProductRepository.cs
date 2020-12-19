aaac373c55f8ab9e1875ea0d1882dc98fac20115
amir.jhanem93 <amir.jhanem93@gmail.com>
2020-12-19 21:35:38 +0200

    Create the product repository

---
+++ EShop.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using EShop.Core.Entities;
+using EShop.Core.Repositories.Core;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EShop.Core.Repositories
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        Task<IEnumerable<Product>> GetProductListAsync();
+        Task<IEnumerable<Product>> GetProductByNameAsync(string productName);
+    }
+}
---
+++ EShop.Infrastructure/Repository/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using EShop.Core.Entities;
+using EShop.Core.Repositories;
+using EShop.Infrastructure.Data;
+using EShop.Infrastructure.Repository.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Infrastructure.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(EShopDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Product>> GetProductListAsync()
+        {
+            return await GetAllAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByNameAsync(string productName)
+        {
+            return await GetAsync(x => x.Name.ToLower().Contains(productName.ToLower()));
+        }
+    }
+}
