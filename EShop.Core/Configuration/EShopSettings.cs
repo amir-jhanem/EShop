9740cd48efdeed29e1778fb1d625a2e1842a2f02
amir.jhanem93 <amir.jhanem93@gmail.com>
2020-12-19 20:15:24 +0200

    Create EShop settings

---
+++ EShop.Core/Configuration/EShopSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Core.Configuration
+{
+    public class EShopSettings
+    {
+        public string ConnectionString { get; set; }
+    }
+}
