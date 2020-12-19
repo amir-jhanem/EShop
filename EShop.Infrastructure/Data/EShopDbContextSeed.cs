96fbebcc8a2d824b309f5a193b83ef46e9c6a7be
amir.jhanem93 <amir.jhanem93@gmail.com>
2020-12-19 22:13:23 +0200

    Seed more products

--- EShop.Infrastructure/Data/EShopDbContextSeed.cs
+++ EShop.Infrastructure/Data/EShopDbContextSeed.cs
@@ -42,62 +42,33 @@ namespace EShop.Infrastructure.Data
 
             var products = new List<Product>
             {
-                // Phone
                 new Product()
                 {
-                    Name = "uPhone X",
-                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
-                    UnitPrice = 295,
+                    Name = "IPhone - Black",
+                    ImageFile = "https://cdn.pixabay.com/photo/2013/07/12/18/39/smartphone-153650_960_720.png",
+                    UnitPrice = 150,
                     UnitsInStock = 10
                 },
                 new Product()
                 {
-                    Name = "uPhone X",
-                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
-                    UnitPrice = 295,
-                    UnitsInStock = 10
+                    Name = "PC Monitor",
+                    ImageFile = "https://cdn.pixabay.com/photo/2012/04/13/15/03/monitor-32743_960_720.png",
+                    UnitPrice = 200,
+                    UnitsInStock = 5
                 },
                 new Product()
                 {
-                    Name = "uPhone X",
-                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
-                    UnitPrice = 295,
-                    UnitsInStock = 10
+                    Name = "Mac Collection",
+                    ImageFile = "https://cdn.pixabay.com/photo/2017/05/11/11/15/workplace-2303851_960_720.jpg",
+                    UnitPrice = 900,
+                    UnitsInStock = 6
                 },
                 new Product()
                 {
-                    Name = "uPhone X",
-                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
-                    UnitPrice = 295,
-                    UnitsInStock = 10
-                },
-                new Product()
-                {
-                    Name = "uPhone X",
-                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
-                    UnitPrice = 295,
-                    UnitsInStock = 10
-                },
-                new Product()
-                {
-                    Name = "uPhone X",
-                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
-                    UnitPrice = 295,
-                    UnitsInStock = 10
-                },
-                new Product()
-                {
-                    Name = "uPhone X",
-                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
-                    UnitPrice = 295,
-                    UnitsInStock = 10
-                },
-                new Product()
-                {
-                    Name = "uPhone X",
-                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
-                    UnitPrice = 295,
-                    UnitsInStock = 10
+                    Name = "Keyboard",
+                    ImageFile = "https://cdn.pixabay.com/photo/2013/07/13/11/50/computer-158770_960_720.png",
+                    UnitPrice = 1000,
+                    UnitsInStock = 20
                 }
             };
 
