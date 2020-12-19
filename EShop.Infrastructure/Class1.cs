871f5155309e8455426f22c92ae34bf77e99ec25
amir.jhanem93 <amir.jhanem93@gmail.com>
2020-12-19 20:16:03 +0200

    Initiate EShop DbContext

--- EShop.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace EShop.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
---
+++ EShop.Infrastructure/Data/EShopDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Infrastructure.Data
+{
+    public class EShopDbContext : IdentityDbContext<IdentityUser>
+    {
+        public EShopDbContext(DbContextOptions<EShopDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
