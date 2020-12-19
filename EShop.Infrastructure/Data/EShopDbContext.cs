d09b89f3949ec4b05468bc3df5ce4b916cbb577e
amir.jhanem93 <amir.jhanem93@gmail.com>
2020-12-19 21:37:16 +0200

    Seed products

--- EShop.Infrastructure/Data/EShopDbContext.cs
+++ EShop.Infrastructure/Data/EShopDbContext.cs
@@ -1,8 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using EShop.Core.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace EShop.Infrastructure.Data
@@ -14,9 +13,21 @@ namespace EShop.Infrastructure.Data
         {
         }
 
+        public DbSet<Product> Products { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            SetTableNamesAsSingle(builder);
+
             base.OnModelCreating(builder);
+        }
+
+        private static void SetTableNamesAsSingle(ModelBuilder builder)
+        {
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                builder.Entity(entityType.ClrType).ToTable(entityType.ClrType.Name);
+            }
         }
     }
 }
---
+++ EShop.Infrastructure/Data/EShopDbContextSeed.cs
@@ -0,0 +1,108 @@
+﻿using EShop.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Infrastructure.Data
+{
+    public class EShopDbContextSeed
+    {
+        public static async Task SeedAsync(EShopDbContext eshopDbContext, ILoggerFactory loggerFactory, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+
+            try
+            {
+                eshopDbContext.Database.Migrate();
+                eshopDbContext.Database.EnsureCreated();
+
+                // products
+                await SeedProductsAsync(eshopDbContext);
+            }
+            catch (Exception exception)
+            {
+                if (retryForAvailability < 10)
+                {
+                    retryForAvailability++;
+                    var log = loggerFactory.CreateLogger<EShopDbContext>();
+                    log.LogError(exception.Message);
+                    await SeedAsync(eshopDbContext, loggerFactory, retryForAvailability);
+                }
+                throw;
+            }
+        }
+
+        private static async Task SeedProductsAsync(EShopDbContext eshopDbContext)
+        {
+            if (eshopDbContext.Products.Any())
+                return;
+
+            var products = new List<Product>
+            {
+                // Phone
+                new Product()
+                {
+                    Name = "uPhone X",
+                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
+                    UnitPrice = 295,
+                    UnitsInStock = 10
+                },
+                new Product()
+                {
+                    Name = "uPhone X",
+                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
+                    UnitPrice = 295,
+                    UnitsInStock = 10
+                },
+                new Product()
+                {
+                    Name = "uPhone X",
+                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
+                    UnitPrice = 295,
+                    UnitsInStock = 10
+                },
+                new Product()
+                {
+                    Name = "uPhone X",
+                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
+                    UnitPrice = 295,
+                    UnitsInStock = 10
+                },
+                new Product()
+                {
+                    Name = "uPhone X",
+                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
+                    UnitPrice = 295,
+                    UnitsInStock = 10
+                },
+                new Product()
+                {
+                    Name = "uPhone X",
+                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
+                    UnitPrice = 295,
+                    UnitsInStock = 10
+                },
+                new Product()
+                {
+                    Name = "uPhone X",
+                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
+                    UnitPrice = 295,
+                    UnitsInStock = 10
+                },
+                new Product()
+                {
+                    Name = "uPhone X",
+                    ImageFile = "https://i.imgur.com/Te1vdCG.png",
+                    UnitPrice = 295,
+                    UnitsInStock = 10
+                }
+            };
+
+            eshopDbContext.Products.AddRange(products);
+            await eshopDbContext.SaveChangesAsync();
+        }
+    }
+}
