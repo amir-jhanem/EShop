7bfd543223c13a048796fe9bdbdcc4353ce330a5
amir.jhanem93 <amir.jhanem93@gmail.com>
2020-12-19 20:17:06 +0200

    Initiate Eshop Identity scheme

--- EShop.Web/Startup.cs
+++ EShop.Web/Startup.cs
@@ -1,6 +1,10 @@
+using EShop.Core.Configuration;
+using EShop.Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,7 +27,11 @@ namespace EShop.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            // eshop dependencies
+            ConfigureEShopServices(services);
+
+            services.AddRazorPages()
+                .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,11 +53,46 @@ namespace EShop.Web
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+            });
+        }
+
+        public void ConfigureEShopServices(IServiceCollection services)
+        {
+            // Add Core Layer
+            services.Configure<EShopSettings>(Configuration);
+
+            // Add Infrastructure Layer
+            ConfigureDatabases(services);
+            ConfigureIdentity(services);
+        }
+
+        public void ConfigureDatabases(IServiceCollection services)
+        {
+            services.AddDbContext<EShopDbContext>(c =>
+                c.UseSqlite(Configuration.GetConnectionString("EShopDbContextConnection"), x => x.MigrationsAssembly("EShop.Web")));
+        }
+
+        public void ConfigureIdentity(IServiceCollection services)
+        {
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddDefaultUI()
+                .AddEntityFrameworkStores<EShopDbContext>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
             });
         }
     }
